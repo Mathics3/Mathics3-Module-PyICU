daa43db66a6fd7395ac0ad5fefe95eb65ca5de91
rocky <rb@dustyfeet.com>
2025-09-02 11:18:43 -0400

    Convert to 9.0.0 API

--- pymathics/language/__main__.py
+++ pymathics/language/__main__.py
@@ -12,7 +12,7 @@ Languages & Translations
 from icu import Locale, LocaleData
 from typing import List, Optional
 
-from mathics.builtin.base import Builtin
+from mathics.core.builtin import Builtin
 from mathics.core.atoms import String
 from mathics.core.convert.expression import to_mathics_list
 
@@ -27,6 +27,7 @@ def eval_alphabet(language_name: String) -> Optional[List[String]]:
 
     py_language_name = language_name.value
     locale = language2locale.get(py_language_name, py_language_name)
+    print(locale)
     if locale not in availableLocales:
         return
     alphabet_set = LocaleData(locale).getExemplarSet(0, 0)
@@ -70,7 +71,7 @@ class Alphabet(Builtin):
 
     summary_text = "lowercase letters in an alphabet"
 
-    def apply(self, alpha: String, evaluation):
+    def eval(self, alpha: String, evaluation):
         """Alphabet[alpha_String]"""
         alphabet_list = eval_alphabet(alpha)
         if alphabet_list is None:
